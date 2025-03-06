abbac20779e57b8010f9d275cc849edab332a7d6
sunil55999 <46947290+sunil55999@users.noreply.github.com>
2025-03-06 13:10:19 +0530

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,12 +1,15 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'hms');
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+define('DB_SERVER', 'trolley.proxy.rlwy.net'); // Railway MySQL host
+define('DB_USER', 'root'); // Railway MySQL username
+define('DB_PASS', 'bHfEAViCgGwleFihLjvuCIpZYUFseDTx'); // Railway MySQL password
+define('DB_NAME', 'railway'); // Railway MySQL database name
+define('DB_PORT', 3306); // Railway MySQL port
+
+// Create connection
+$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+
 // Check connection
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (!$con) {
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 ?>
