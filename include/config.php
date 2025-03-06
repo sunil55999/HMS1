b4fb79b8c3020112b349b5f75b554c0ab0c7b194
sunil55999 <46947290+sunil55999@users.noreply.github.com>
2025-03-06 15:44:35 +0530

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,9 +1,9 @@
 <?php
-define('DB_SERVER', 'mysql.railway.internal'); // Railway MySQL host
+define('DB_SERVER', 'trolley.proxy.rlwy.net'); // Railway MySQL host
 define('DB_USER', 'root'); // Railway MySQL username
 define('DB_PASS', 'bHfEAViCgGwleFihLjvuCIpZYUFseDTx'); // Railway MySQL password
 define('DB_NAME', 'railway'); // Railway MySQL database name
-define('DB_PORT', 3306); // Railway MySQL port
+define('DB_PORT', 16391); // Railway MySQL port
 
 // Create connection
 $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
